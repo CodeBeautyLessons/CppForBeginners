ac70cbe03a147338ef580fc3c6bf317dfa19afb2
Vladimir Novikov <novikovvlal@gmail.com>
2022-02-24 21:57:18 +0300

    Добавьте файлы проекта.

---
+++ CppForBeginners/CppForBeginners.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <ctime>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "My name is Vova. ";
+
+    time_t tt;
+
+    struct tm* ti = new tm();
+
+    time(&tt);
+
+    localtime_s(ti, & tt);
+
+    std::cout << "Today is " << ti->tm_wday << " day of the week.";
+
+
+    system("pause>0");
+}
